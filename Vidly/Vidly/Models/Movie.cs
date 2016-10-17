e36728da6cb80a24e50e870dc0d4d35d23ccfb4c
CornellEmperio <cornell@emperiogroup.co.za>
2016-10-17 09:55:57 +0200

    Add project files.

---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
